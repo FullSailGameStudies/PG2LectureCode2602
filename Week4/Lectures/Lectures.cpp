1f9169a5fe1eaf691f0dd51bd91e0a8287348303
Garrett Girod <girodg@hotmail.com>
2026-02-23 10:45:09 -0500

    file I/O

--- Week4/Lectures/Lectures.cpp
+++ Week4/Lectures/Lectures.cpp
@@ -3,10 +3,90 @@
 #include "Day10.h"
 #include "Day11.h"
 #include "Input.h"
+#include <fstream> //for ifstream, ofstream
+#include <sstream> //for stringstream
 
 
 int main(int argc, char* args[])
 {
+	std::string fileName = "2602.csv";
+	std::string path = "C:/temp/2602/";
+	std::string finalPath = path + fileName;
+	//1. open the file
+	//	check if the file is open
+	std::ofstream outFile(finalPath);
+	char delimiter = '$';
+	if (outFile.is_open())
+	{
+		//2. write to the file
+		//  << insertion operator
+		outFile << "Batman rules!" << delimiter;
+		outFile << 12 << delimiter << 5.371 << delimiter << true;
+		outFile << delimiter << "Aquaman smells.";
+	}
+	else
+	{
+		std::cout << finalPath << " could not be opened.\n";
+	}
+	//3. CLOSE the file
+	//	good programming practice: close the file ASAP!
+	outFile.close();
+
+	std::ifstream inFile(finalPath);
+	if (inFile.is_open())
+	{
+		//check the inFile.eof()
+		// eof - end of file
+		// 
+		//2. read from the file
+		//  << insertion operator
+		// 	
+		std::string line;
+		//reads the infile UNTIL it reaches 
+		// a newline OR the end of the file
+		std::getline(inFile, line);
+		std::cout << line << "\n";
+		//parse the string
+		std::string data;
+		std::stringstream lineStream(line);
+		//reads the stream UNTIL it reaches 
+		// a delimiter OR the end of the stream
+		std::getline(lineStream, data, delimiter);
+		std::cout << data << "\n";
+
+		std::getline(lineStream, data, delimiter);
+		int iData = std::stoi(data);
+		std::cout << iData << "\n";
+
+		std::getline(lineStream, data, delimiter);
+		double dData = std::stod(data);
+		std::cout << dData << "\n";
+
+		try
+		{
+			std::getline(lineStream, data, delimiter);
+			bool bData = std::stoi(data);
+			std::cout << bData << "\n";
+		}
+		catch (const std::exception& ex)
+		{
+			std::cout << "Error processing the boolean\n";
+			std::cout << "DATA: " << data << "\n";
+			std::cout << ex.what() << "\n";
+		}		//try-catch
+
+		std::getline(lineStream, data, delimiter);
+		std::cout << data << "\n";
+	}
+	else
+	{
+		std::cout << finalPath << " could not be opened.\n";
+	}
+	//3. CLOSE the file
+	//	good programming practice: close the file ASAP!
+	inFile.close();
+
+
     std::string hello = "Hello Week 4!";
     for (auto& ch : hello)
     {
